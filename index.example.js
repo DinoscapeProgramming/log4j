30d5fdd0e2f4dd13d760c70490f1e91eaebed087
DinoscapeProgramming <95239392+DinoscapeProgramming@users.noreply.github.com>
2022-02-24 13:36:23 +0100

    Update index.example.js

--- index.example.js
+++ index.example.js
@@ -1,3 +1,10 @@
 const log = require('log4j.js');
+const settings = {
+  first: "test",
+  second: "package",
+  third: "nice"
+};
 
 log("Hey {}, what's {}. Welcome to {}. Have {}!", ["guys", "up", "log4j", "fun"])
+
+log("Hey, this is a {}. This {} is {}!", [settings.first, settings.second, settings.third])
